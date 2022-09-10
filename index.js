175d7561d65eb10a4d54ee6c0a3943b1d78a8a4d
Martin <martin.capovcak@gmail.com>
2022-09-10 11:48:28 +0200

    reformating

--- index.js
+++ index.js
@@ -2,16 +2,30 @@
 
 //Dependencies
 
+//Modules
+const readline = require('readline')
+
 //Imports
-const pkgJSON = require('./package.json');
-const init = require('./utils/init');
-const log = require('./utils/helpers/logs');
+const pkgJSON = require('./package.json')
+const init = require('./utils/init')
+const log = require('./utils/helpers/logs')
 
 //Constants
+const args = process.argv.slice(2)
+const rl = readline.createInterface({
+	input: process.stdin,
+	output: process.stdout
+})
 
 // Logic
-(() => {
-	init();
+;(() => {
+	init()
 
-	log('Whale Tracker');
-})(); // IFIE foo
+	log('Whale Tracker')
+	console.log('args', args)
+
+	rl.question('What do you think of Node.js? ', answer => {
+		console.log('Thank you for your feedback: ', answer)
+		rl.close()
+	})
+})() // IFIE foo
--- utils/init.js
+++ utils/init.js
@@ -3,15 +3,16 @@ const pkgJSON = require('../package.json');
 const log = require('./helpers/logs');
 
 //Constants
-
-const init = () => {
-	console.clear();
-
-	log(`
+const head = `
 Name        : ${pkgJSON.name}
 Version     : ${pkgJSON.version}
 Description : ${pkgJSON.description}
-`);
+`;
+
+// foo
+const init = () => {
+	console.clear();
+	log(head);
 };
 
 module.exports = init;

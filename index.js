ff255fd2a09485f8358d71d2d6e3f32525af6026
Martin <martin.capovcak@gmail.com>
2022-09-13 01:57:19 +0200

    err handling

--- index.js
+++ index.js
@@ -15,28 +15,36 @@ const { prompt, defaultResponse } = require('./utils/prompts.js')
 const cli = require('./utils/cli.js')
 const sonar = require('./utils/sonar.js')
 
+const alert = require('./utils/helpers/alerts.js')
+const handleError = require('./utils/helpers/handle-errors')
+const unhandled = require('./utils/helpers/handle-unhandled.js')
+
 //Constants
 //const args = process.argv.slice(2)
 const input = cli.input
 const flags = cli.flags
 
+unhandled()
+//Promise.reject(new Error('THIS_IS_UNHANDLED'))
+
 // Logic
 ;(async () => {
 	init()
+
 	input.includes('help') && cli.showHelp(0)
 
 	// temp logs
-	console.log('cli', cli.input)
-	console.log('cli', cli.flags)
+	//console.log('cli', cli.input)
+	//console.log('cli', cli.flags)
 
 	if (cli.flags.default) {
-		sonar(defaultResponse)
+		await sonar(defaultResponse)
 	}
 
 	if (cli.flags.config) {
 		let response = await prompt()
 		if (response.token && response.treshold && response.value) {
-			sonar(response)
+			await sonar(response)
 		} else {
 			console.log('Wrong input')
 		}

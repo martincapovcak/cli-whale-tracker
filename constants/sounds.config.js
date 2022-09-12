01dc1f36d7e2b35465764562f5ff228a1e1d2ac9
Martin <martin.capovcak@gmail.com>
2022-09-12 23:36:52 +0200

    sounds paths

---
+++ constants/sounds.config.js
@@ -0,0 +1,8 @@
+module.exports = {
+	soundFx: {
+		whale: 'assets/sound/whale.mp3',
+		sonar: 'assets/sound/sonar-ping.mp3',
+		beep: 'assets/sound/beep.mp3',
+		clickbeep: 'assets/sound/click-beep.mp3'
+	}
+}
